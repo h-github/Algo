30e45cca3c15bd457833095cf9223a288c93f4d5
h-github <hmohammadi@hotmail.ca>
2018-07-02 15:03:03 -0700

    Passing Cars %100

--- PrefixSums/Program.cs
+++ PrefixSums/Program.cs
@@ -10,14 +10,35 @@ namespace PrefixSums
   {
     static void Main(string[] args)
     {
-
+      int[] A = new int[] { 0, 1, 0, 1, 1 };
+      //List<int> A = new List<int>();
+      //Random r = new Random();
+      //for (Int64 i = 0; i < 1000000000; i++)
+      //{
+      //  A.Add(r.Next(0, 2));
+      //}
+      int answer = PassingCars(A);
     }
 
 
     public static int PassingCars(int[] A)
     {
       int answer = 0;
+      int increment = 0;
 
+      for (int i = 0; i < A.Length; i++)
+      {
+        if (A[i] == 0)
+        {
+          increment++;
+        }
+        else if (A[i] == 1)
+        {
+          answer += increment;
+          if (answer > 1000000000)
+            return -1;
+        }
+      }
       return answer;
     }
   }
