4344e92d19932f9e7ffd51be8908f469095958a2
h-github <hmohammadi@hotmail.ca>
2018-07-03 00:39:37 -0700

    new

--- PrefixSums/Program.cs
+++ PrefixSums/Program.cs
@@ -15,6 +15,55 @@ namespace PrefixSums
 
       //int answer = CountDiv(11, 14, 2);
 
+      int[] P = new int[] { 2, 5, 0 };
+      int[] Q = new int[] { 4, 5, 6 };
+      string S = "CAGCCTA";
+
+      var answer = GenomicRangeQuery(S, P, Q);
+    }
+
+    public static int[] GenomicRangeQuery(string S, int[] P, int[] Q)
+    {
+      int[] minimals = new int[P.Length];
+      int[] impacts = new int[S.Length];
+
+      for (int i = 0; i < S.Length; i++)
+      {
+        int impact = 0;
+
+        switch (S[i])
+        {
+          case 'A':
+            impact = 1;
+            break;
+          case 'C':
+            impact = 2;
+            break;
+          case 'G':
+            impact = 3;
+            break;
+          case 'T':
+            impact = 4;
+            break;
+        }
+
+        impacts[i] = impact;
+      }
+
+      if(impacts.Distinct().Count() == 1)
+      {
+        int elem = impacts.Distinct().First();
+        return Enumerable.Repeat(elem, P.Length).ToArray();
+      }
+
+      for (int i = 0; i < P.Length; i++)
+      {
+        int min = P[i] <= Q[i] ? P[i] : Q[i];
+        int max = P[i] >= Q[i] ? P[i] : Q[i];
+        minimals[i] = impacts.Skip(min).Take(max - min + 1).Min();
+      }
+
+      return minimals;
 
     }
 
