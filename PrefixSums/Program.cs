70dfe5a9ca35acb9ced09a4869b992d16fa59a8f
h-github <hmohammadi@hotmail.ca>
2018-07-02 22:52:12 -0700

    Count Div 100%

--- PrefixSums/Program.cs
+++ PrefixSums/Program.cs
@@ -10,30 +10,54 @@ namespace PrefixSums
   {
     static void Main(string[] args)
     {
-      int[] A = new int[] { 0, 1, 0, 1, 1 };
-      int answer = PassingCars(A);
+      //int[] A = new int[] { 0, 1, 0, 1, 1 };
+      //int answer = PassingCars(A);
+
+      //int answer = CountDiv(11, 14, 2);
+
+
     }
 
+    //public static int CountDiv(int A, int B, int K)
+    //{
+    //  int answer = 0;
 
-    public static int PassingCars(int[] A)
-    {
-      int answer = 0;
-      int increment = 0;
+    //  if (A == B && A % K == 0)
+    //    return 1;
 
-      for (int i = 0; i < A.Length; i++)
-      {
-        if (A[i] == 0)
-        {
-          increment++;
-        }
-        else if (A[i] == 1)
-        {
-          answer += increment;
-          if (answer > 1000000000)
-            return -1;
-        }
-      }
-      return answer;
-    }
+    //  if (A == B && A % K != 0)
+    //    return 0;
+
+    //  //answer += (((B - A) - ((B - A) % K)) / K);
+    //  answer += ((B - (B % K)) - (A - (A % K))) / K;
+
+    //  if (A % K == 0)
+    //  {
+    //    answer++;
+    //  }
+
+    //  return answer;
+    //}
+
+    //public static int PassingCars(int[] A)
+    //{
+    //  int answer = 0;
+    //  int increment = 0;
+
+    //  for (int i = 0; i < A.Length; i++)
+    //  {
+    //    if (A[i] == 0)
+    //    {
+    //      increment++;
+    //    }
+    //    else if (A[i] == 1)
+    //    {
+    //      answer += increment;
+    //      if (answer > 1000000000)
+    //        return -1;
+    //    }
+    //  }
+    //  return answer;
+    //}
   }
 }
